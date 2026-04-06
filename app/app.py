8bf12b0ffd010f9a2e49eba2ce4e603cacb3764f
AjaySubhashSatti <sattiajaysubhash@gmail.com>
2026-04-06 14:28:38 +0530

    fix: resolve flake8 lint errors

--- app/app.py
+++ app/app.py
@@ -3,11 +3,14 @@ import os
 import socket
 from datetime import datetime
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "🚀 DevOps Platform Running"
+
 
 @app.route("/health")
 def health():
@@ -15,11 +18,13 @@ def health():
         "status": "healthy"
     }), 200
 
+
 @app.route("/env")
 def env():
     return jsonify({
         "environment": os.getenv("ENV", "dev")
     })
+
 
 @app.route("/info")
 def info():
@@ -27,6 +32,7 @@ def info():
         "hostname": socket.gethostname(),
         "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
